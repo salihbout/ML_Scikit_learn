29bb4b08fa36d86b511b547ea5e4b87264091800
salihbout <salihb@qq.com>
2017-04-02 11:49:29 +0800

    Perceptron using scikit-learm

---
+++ Perceptron.py
@@ -0,0 +1,66 @@
+from sklearn import datasets
+from sklearn.cross_validation import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import Perceptron
+from sklearn.metrics import accuracy_score
+import matplotlib.pyplot as plt
+from matplotlib.colors import ListedColormap
+import numpy as np
+
+#USeful function to use later : Plotting decision region
+def plot_decision_regions(X, y, classifier, test_idx=None, resolution=0.02):
+    markers = ('s', 'x', 'o', '^', 'v')
+    colors = ('red', 'blue', 'lightgreen', 'gray', 'cyan')
+    cmap = ListedColormap(colors[:len(np.unique(y))])
+    # plot the decision surface
+    x1_min, x1_max = X[:, 0].min() - 1, X[:, 0].max() + 1
+    x2_min, x2_max = X[:, 1].min() - 1, X[:, 1].max() + 1
+    xx1, xx2 = np.meshgrid(np.arange(x1_min, x1_max, resolution),
+                           np.arange(x2_min, x2_max, resolution))
+    Z = classifier.predict(np.array([xx1.ravel(), xx2.ravel()]).T)
+    Z = Z.reshape(xx1.shape)
+    plt.contourf(xx1, xx2, Z, alpha=0.4, cmap=cmap)
+    plt.xlim(xx1.min(), xx1.max())
+    plt.ylim(xx2.min(), xx2.max())
+    # plot class samples
+    X_test, Y_test = X[test_idx, :], y[test_idx]
+    for idx, cl in enumerate(np.unique(y)):
+        plt.scatter(x=X[y == cl, 0], y=X[y == cl, 1],
+                    alpha=0.8, c=cmap(idx),
+                    marker=markers[idx], label=cl)
+    if test_idx :
+        X_test, Y_test = X[test_idx, :], y[test_idx]
+        plt.scatter(X_test[:,0], X_test[:,1], c='o', s=55, label='test set')
+
+#Load the IRIS datasets and prepare model's variables
+iris = datasets.load_iris()
+X= iris.data[:,[2,3]]
+Y = iris.target
+
+#Split our data into train/test set
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.3, random_state=0)
+
+#Standerize our features
+sc = StandardScaler()
+sc.fit(X_train)
+X_train_std = sc.transform(X_train)
+X_test_std = sc.transform(X_test)
+
+#Train our percetron model
+ppn = Perceptron(n_iter=40, eta0=0.1, random_state=0)
+ppn.fit(X_train_std, Y_train)
+
+#Prediction for our test data
+Y_predicted = ppn.predict(X_test_std)
+
+print("Our Perceptron fails to classify : %d" %(Y_test != Y_predicted).sum())
+print('Accuracy: %.2f' %accuracy_score(Y_test, Y_predicted))
+
+X_full_std = np.vstack((X_test_std, X_test_std))
+Y_full = np.hstack((Y_train, Y_test))
+
+plot_decision_regions(X=X_full_std, y=Y_full, classifier=ppn, test_idx=range(105,150))
+plt.xlabel('petal length std')
+plt.ylabel('petal width std')
+plt.legend(loc='upper left')
+plt.show()
